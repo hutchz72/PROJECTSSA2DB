9b69977cfc810cdce54623157050621b8c820326
hutchz72 <110713034+hutchz72@users.noreply.github.com>
2024-05-12 19:19:14 +0700

    Update SSA2DBscriptSprint3.sql

--- SSA2DBscriptSprint3.sql
+++ SSA2DBscriptSprint3.sql
@@ -55,4 +55,6 @@ values (1, 'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitl
 (2, 'Repository', '', '',  2, '2024-04-22 09:05:00', '2024-04-22 14:00:00'),
 (3, 'ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)',  3, '2024-04-22 09:10:00', '2024-04-25 00:00:00'),
 (4, ' _Infrastructure_ ', ' _Setup containers_ ', 'ไก่งวง กับ เพนกวิน',  4, '2024-04-22 09:15:00', '2024-04-22 10:00:00'),
-(5, 'Sample Task 1', 'This is a sample task description', 'John Doe', 2,  NOW(), NOW());
+(5, 'DevSecOps', null, null, null, now(), now()),
+(6, 'DevSecOps1DevSecOps2DevSecOps3DevSecOps4DevSecOps5DevSecOps6DevSecOps7DevSecOps8DevSecOps9DevSecOps0','Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 'Assignees1Assignees2Assignees3', 2, now(), now()),
+(7, '     _Front End_     ', '     _fix CSS on landing page_     ', '     _ปุยฝ้าย กับ เสือดาว_     ', 4, now(), now());
